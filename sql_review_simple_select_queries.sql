9aa8386058a85c6a4e7a0c3ee9ad0234355f0cc1
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-10-20 00:28:09 +0600

    Add review lesson solution

---
+++ sql_review_simple_select_queries.sql
@@ -0,0 +1,33 @@
+-- Review 1 — Tasks
+
+-- 1. List all the Canadian cities and their populations
+SELECT city,
+       population
+FROM   north_american_cities
+WHERE  country = 'Canada';
+
+-- 2. Order all the cities in the United States by their latitude from north to south
+SELECT city
+FROM   north_american_cities
+WHERE  country = 'United States'
+ORDER  BY latitude DESC;
+
+-- 3. List all the cities west of Chicago, ordered from west to east
+SELECT city
+FROM   north_american_cities
+ORDER  BY longitude
+LIMIT  6;
+
+-- 4. List the two largest cities in Mexico (by population)
+SELECT city
+FROM   north_american_cities
+WHERE  country = 'Mexico'
+ORDER  BY population DESC
+LIMIT  2;
+
+-- 5. List the third and fourth largest cities (by population) in the United States and their population
+SELECT city
+FROM   north_american_cities
+WHERE  country = 'United States'
+ORDER  BY population DESC
+LIMIT  2 offset 2;
