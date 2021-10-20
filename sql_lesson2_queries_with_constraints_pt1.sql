bfbc6dcc0ffe3f77d2eb9b81aa4e7362eb42a993
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-10-20 14:46:58 +0600

    Update sql_lesson2_queries_with_constraints_pt1.sql

--- sql_lesson2_queries_with_constraints_pt1.sql
+++ sql_lesson2_queries_with_constraints_pt1.sql
@@ -1,17 +1,14 @@
 -- Exercise 2 — Tasks
-SELECT *
-FROM   movies
-WHERE  id = 6;
 
 -- 1. Find the movie with a row id of 6
 SELECT *
 FROM   movies
-WHERE  year BETWEEN 2000 AND 2010;  
+WHERE  id = 6;
 
 -- 2. Find the movies released in the years between 2000 and 2010
 SELECT *
 FROM   movies
-WHERE  year BETWEEN 2000 AND 2010;  
+WHERE  year BETWEEN 2000 AND 2010;
 
 -- 3. Find the movies not released in the years between 2000 and 2010
 SELECT *
