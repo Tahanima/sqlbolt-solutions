8923969b8c594869b2b9419a1c9ddfad85c23a90
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-10-26 15:49:03 +0600

    Add Lesson 18 solutions

---
+++ sql_lesson18_dropping_tables.sql
@@ -0,0 +1,7 @@
+-- Exercise 18 — Tasks
+
+-- 1. We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+DROP TABLE movies;
+
+-- 2. And drop the BoxOffice table as well
+DROP TABLE boxoffice;
